e867406d91df80354937dd806531b5a3b5589423
pakhomn <tasha.pakhom@gmail.com>
2023-09-28 20:37:08 +0300

    commit message

---
+++ app/src/main/java/com/example/playlistmaker2023/SearchActivity.kt
@@ -0,0 +1,11 @@
+package com.example.playlistmaker2023
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
