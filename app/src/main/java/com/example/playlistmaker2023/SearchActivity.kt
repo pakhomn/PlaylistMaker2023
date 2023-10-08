fe71facdb03e8d07ee3be2db41c709ec249f5d9c
pakhomn <tasha.pakhom@gmail.com>
2023-10-08 16:37:37 +0300

    dev3 v1

--- app/src/main/java/com/example/playlistmaker2023/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker2023/SearchActivity.kt
@@ -1,11 +1,19 @@
 package com.example.playlistmaker2023
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar.setNavigationOnClickListener  {
+            val displayIntent = Intent(this, MainActivity::class.java)
+            startActivity(displayIntent)
+        }
     }
 }
--- app/src/main/java/com/example/playlistmaker2023/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker2023/SettingsActivity.kt
@@ -1,7 +1,9 @@
 package com.example.playlistmaker2023
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -24,11 +26,60 @@ internal class SettingsActivity : AppCompatActivity() {
          */
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
-
         toolbar.setNavigationOnClickListener  {
             val displayIntent = Intent(this, MainActivity::class.java)
             startActivity(displayIntent)
         }
 
+        val buttonShare = findViewById<ImageView>(R.id.button_share)
+        buttonShare.setOnClickListener {
+            shareApp()
+        }
+
+        val buttonSupport = findViewById<ImageView>(R.id.button_support)
+        buttonSupport.setOnClickListener {
+            writeToSupport()
+        }
+
+        val buttonUserAgreement = findViewById<ImageView>(R.id.button_user_agreement)
+        buttonUserAgreement.setOnClickListener {
+            moveToOffertInBrowser()
+        }
+
+    }
+
+    private fun shareApp() {
+        val sendIntent = Intent()
+        sendIntent.action = Intent.ACTION_SEND
+        val appLink = getString(R.string.link_for_sharing)
+        val share = getString(R.string.share)
+        sendIntent.putExtra(Intent.EXTRA_TEXT, appLink)
+        sendIntent.type = "text/plain"
+
+        val shareIntent = Intent.createChooser(sendIntent, share)
+        startActivity(shareIntent)
+    }
+
+    private fun writeToSupport() {
+        val emailAddress = arrayOf(getString(R.string.support_email))
+        val subject = getString(R.string.support_email_subject)
+        val message = getString(R.string.support_email_text)
+
+        val emailIntent = Intent(Intent.ACTION_SENDTO)
+
+        emailIntent.data = Uri.parse("mailto:")
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, emailAddress)
+        emailIntent.putExtra(Intent.EXTRA_TEXT, message)
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+
+        startActivity(emailIntent)
+    }
+
+    private fun moveToOffertInBrowser() {
+
+        val userAgreementLink = getString(R.string.user_agreement_link)
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(userAgreementLink))
+
+        startActivity(browserIntent)
     }
 }
