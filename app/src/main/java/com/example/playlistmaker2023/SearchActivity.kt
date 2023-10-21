d46d1bec74f4d244ec7d521de0e3ba326ab59656
pakhomn <tasha.pakhom@gmail.com>
2023-10-21 09:18:40 +0300

    dev4 with review comments

--- app/src/main/java/com/example/playlistmaker2023/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker2023/SearchActivity.kt
@@ -17,28 +17,46 @@ class SearchActivity : AppCompatActivity() {
     private lateinit var searchEditText: EditText
     private lateinit var clearButton: ImageView
     private lateinit var cursor: ImageView
-    var searchText: String = ""
+    private var searchText: String = ""
+
+    companion object {
+        private const val SEARCH_TEXT_KEY = "searchText"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        initializeViews()
+        setupListeners()
+        restoreSavedState(savedInstanceState)
+    }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_TEXT_KEY, searchText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchText = savedInstanceState.getString(SEARCH_TEXT_KEY, "")
+    }
+
+    private fun initializeViews() {
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        toolbar.setNavigationOnClickListener  {
+        toolbar.setNavigationOnClickListener {
             val displayIntent = Intent(this, MainActivity::class.java)
             startActivity(displayIntent)
         }
 
         searchEditText = findViewById(R.id.searchEditText)
         clearButton = findViewById(R.id.button_clear)
-        //cursor = findViewById(R.id.cursor)
-
         clearButton.visibility = View.GONE
-        //cursor.visibility = View.VISIBLE
+    }
 
+    private fun setupListeners() {
         searchEditText.setOnFocusChangeListener { _, hasFocus ->
             if (hasFocus) {
                 showKeyboard()
-                //cursor.visibility = View.GONE
             } else {
                 hideKeyboard()
             }
@@ -48,18 +66,16 @@ class SearchActivity : AppCompatActivity() {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 // Не реализуем, оставляем как есть
             }
+
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 // Не реализуем, оставляем как есть
             }
 
             override fun afterTextChanged(s: Editable?) {
-                // Если ничего не написали, скрываем кнопку х, иначе - показываем
                 if (s.toString().isNotEmpty()) {
                     clearButton.visibility = View.VISIBLE
-                    //cursor.visibility = View.GONE
                 } else {
                     clearButton.visibility = View.GONE
-                    //cursor.visibility = View.VISIBLE
                 }
                 searchText = s.toString()
             }
@@ -71,19 +87,13 @@ class SearchActivity : AppCompatActivity() {
             searchEditText.text.clear()
             hideKeyboard()
         }
-
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putString("searchText", searchText)
+    private fun restoreSavedState(savedInstanceState: Bundle?) {
+        if (savedInstanceState != null) {
+            searchText = savedInstanceState.getString(SEARCH_TEXT_KEY, "")
+        }
     }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        searchText = savedInstanceState.getString("searchText", "")
-    }
-
 
     private fun showKeyboard() {
         searchEditText.requestFocus()
@@ -94,13 +104,4 @@ class SearchActivity : AppCompatActivity() {
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(searchEditText.windowToken, 0)
     }
-
-    /* первое плохое решение, с фиксом
-    private fun isClearButtonClicked(event: android.view.MotionEvent): Boolean {
-        val drawableRight = 2
-        val x = event.x.toInt()
-        val width = searchEditText.width
-        return x >= width - searchEditText.compoundDrawables[drawableRight].bounds.width()
-    }
-     */
 }
