932cc22c2cb45d181a97b3eb2277556644766dd1
pakhomn <tasha.pakhom@gmail.com>
2023-11-14 08:47:51 +0300

    dev6 sprint 11 - HW

--- app/src/main/java/com/example/playlistmaker2023/Track.kt
+++ app/src/main/java/com/example/playlistmaker2023/Track.kt
@@ -3,6 +3,6 @@ package com.example.playlistmaker2023
 class Track (
     val trackName: String,
     val artistName: String,
-    val trackTime: String,
+    val trackTimeMillis: Long,
     val artworkUrl100: String
 )
